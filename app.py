fcec2ba538bc953de2152ae36820bf4c286e9eb3
YOGESHKUMAR VASANTBHAI PATEL <77333240+yugpatelyp2@users.noreply.github.com>
2021-08-17 10:07:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+import numpy as np
+import pickle
 
 app = Flask(__name__)
+model = pickle.load(open('model.pkl','rb'))
 
 @app.route("/")
+def home():
+    return rander_template('index.html')
 def index():
     return "Welcome To UK Accidents Prediction Matching Learning Modal"
